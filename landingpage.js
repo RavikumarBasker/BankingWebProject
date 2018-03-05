4c6d73e19e3c7dee67900beaca35c4c895c6e1b5
AHM\RBasker <ravikumarbasker@gmail.com>
2018-03-05 17:24:51 -0500

    practice on buttons and container

--- landingpage.js
+++ landingpage.js
@@ -2,3 +2,11 @@
 function displayPage() {
   document.getElementById('personalpage').innerHTML = "You are inside Personal Page";
 }
+
+function displaySmallBusinessPage() {
+  var i=1;
+  var j=3;
+  var k=i+j;
+  console.log(k);
+  // document.body.style.backgroundImage = "url('./staticcontents/smallbusinesspageimage.jpg')";
+}
